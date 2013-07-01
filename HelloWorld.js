0ac2ada2bbe36af7aba4b47be97db824315f856c
OpenShift guest <mazda@aduki.co.uk>
2013-07-01 13:02:20 -0400

    Chnaged to heroku example

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,5 +1,12 @@
-var http = require('http');
-http.createServer(function (req, res) {
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('Hello World\n');
-}).listen(process.env.PORT);
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
